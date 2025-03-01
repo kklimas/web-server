d71dda93d423476d204ec5bf90eee7570e356d6b
kklimas <klimaskacper00@gmail.com>
2025-03-01 14:47:25 +0100

    Jenkins job

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -2,32 +2,34 @@ node {
     def commit = env.GIT_COMMIT
     def branch = params.BRANCH ?: 'develop'
 
-    stage('Checkout') {
-        def scmVariables = checkout([
-                $class: 'GitSCM',
-                branches: [[name: "*/${branch}"]],
-                userRemoteConfigs: [[url: 'https://github.com/kklimas/web-server.git']]
-        ])
-        commit = scmVariables.GIT_COMMIT
-        echo "Triggered pipeline for branch ${branch} and commit ${commit}"
-    }
+    docker.build("devops/web-server:${commit}")
 
-    stage('Build') {
-        echo "Building from branch: ${branch}"
-        sh "./gradlew build"
-    }
-
-    stage('Unit Tests') {
-        echo "Running tests..."
-        sh './gradlew test'
-        echo "Archiving test results..."
-        junit '**/build/test-results/test/*.xml'
-    }
-
-    stage('Docker build') {
-        echo "Building docker image..."
-        script {
-            docker.build("devops/web-server:${commit}")
-        }
-    }
+//    stage('Checkout') {
+//        def scmVariables = checkout([
+//                $class: 'GitSCM',
+//                branches: [[name: "*/${branch}"]],
+//                userRemoteConfigs: [[url: 'https://github.com/kklimas/web-server.git']]
+//        ])
+//        commit = scmVariables.GIT_COMMIT
+//        echo "Triggered pipeline for branch ${branch} and commit ${commit}"
+//    }
+//
+//    stage('Build') {
+//        echo "Building from branch: ${branch}"
+//        sh "./gradlew build"
+//    }
+//
+//    stage('Unit Tests') {
+//        echo "Running tests..."
+//        sh './gradlew test'
+//        echo "Archiving test results..."
+//        junit '**/build/test-results/test/*.xml'
+//    }
+//
+//    stage('Docker build') {
+//        echo "Building docker image..."
+//        script {
+//            docker.build("devops/web-server:${commit}")
+//        }
+//    }
 }
